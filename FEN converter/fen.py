f07285b4e64ec9c43bf39214f7e6e5097952bbca
judahhooper <judahhooper@icloud.com>
2026-07-04 07:34:17 +0100

    update position.py to include self

--- FEN converter/fen.py
+++ FEN converter/fen.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import Position
+
 def parse_fen():
     print("provide FEN string: ")
     # example fen:
--- position.py
+++ position.py
@@ -1,25 +1,28 @@
 class Position:
+    def __init__(self):
+        # having initially defined the bitboards and position metadata
+        # without self, I realised I'd be essentially updating the "blueprint"
+        # rather than the specific instance - self is necessary here. 
 
-    white_pawns = 0
-    white_knights = 0
-    white_bishops = 0
-    white_rooks = 0
-    white_queen = 0
-    white_king = 0
+        
+        # Bitboards
+        self.white_pawns = 0
+        self.white_knights = 0
+        self.white_bishops = 0
+        self.white_rooks = 0
+        self.white_queens = 0
+        self.white_king = 0
 
-    black_pawns = 0
-    black_knights = 0
-    black_bishops = 0
-    black_rooks = 0
-    black_queen = 0
-    black_king = 0
+        self.black_pawns = 0
+        self.black_knights = 0
+        self.black_bishops = 0
+        self.black_rooks = 0
+        self.black_queens = 0
+        self.black_king = 0
 
-    active_colour = white
-
-    castling_availability = KQkq
-
-    en_passant_target_square = -
-
-    halfmove_clock = 0
-
-    fullmove_number = 0
+        # Position metadata
+        self.active_colour = "w"
+        self.castling_rights = ""
+        self.en_passant = None
+        self.halfmove_clock = 0
+        self.fullmove_number = 1
